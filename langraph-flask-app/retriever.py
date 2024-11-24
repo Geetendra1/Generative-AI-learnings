103f62ff784b767f49f9e56192cdbdcb0dbd613f
Geetendra1 <sengargeetendra123@gmail.com>
2024-11-24 12:37:34 +0530

    added multirepresentation and hierarchical indexiing

--- langraph-flask-app/retriever.py
+++ langraph-flask-app/retriever.py
@@ -2,7 +2,6 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 
 
-
 def create_retriever(index_name="gen-ai"):
     vector_store = PineconeVectorStore(index_name=index_name, embedding=OpenAIEmbeddings(model="text-embedding-3-large"))
     retriever = vector_store.as_retriever()
